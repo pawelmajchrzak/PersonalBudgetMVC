a97ff6618d52540cbe2f65f65160a4a9b1b54186
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-02-13 21:11:31 +0100

    add flash messages to login

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\User;
 use \App\Auth;
+use \App\Flash;
 
 /**
  * Login controller
@@ -37,9 +38,12 @@ class Login extends \Core\Controller
 
             Auth::login($user);
 
+            Flash::addMessage('Logowanie zakończone sukcesem');
+
             $this->redirect(Auth::getReturnToPage());
 
         } else {
+            Flash::addMessage('Logowanie nie powiodło się, spróbuj ponownie', Flash::WARNING);
 
             View::renderTemplate('Login/new.html', [
                 'email' => $_POST['email'],
@@ -55,6 +59,21 @@ class Login extends \Core\Controller
     public function destroyAction()
     {
         Auth::logout();
+
+        $this->redirect('/login/show-logout-message');
+    }
+
+
+    /**
+     * Show a "logged out" flash message and redirect to the homepage. Necessary to use the flash messages
+     * as they use the session and at the end of the logout method (destroyAction) the session is destroyed
+     * so a new action needs to be called in order to use the session.
+     *
+     * @return void
+     */
+    public function showLogoutMessageAction()
+    {
+        Flash::addMessage('Wylogowano');
 
         $this->redirect('/');
     }
