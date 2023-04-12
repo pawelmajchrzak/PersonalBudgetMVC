de7985fce396272e6fa890b45eaab10660264d2c
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-04-12 22:57:59 +0200

    createCollapseWithSingleIncomeAndExpenseInViewBalanceSheet

--- public/js/javascrip.js
+++ public/js/javascrip.js
@@ -21,17 +21,8 @@
 
   var exampleModal = document.getElementById('editCategoryIncome')
 exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
   var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
   var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-
   var modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-  
   modalBodyInput.value = recipient
 })
